4e5c4ee368c91ac9628ffae935970543b6806e64
Vasilena Todorova <134315528+vasilenaph@users.noreply.github.com>
2025-04-27 11:47:51 +0300

    Add create route

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeController = require('./src/controllers/homeController');
+const cameraController = require('./src/controllers/cameraController');
 
 router.use(homeController);
+router.use(cameraController);
 
 module.exports = router;
