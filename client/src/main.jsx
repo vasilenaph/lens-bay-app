7e3fc90147a127df1c70108d080909f7a6e84dce
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2025-08-17 09:15:14 +0300

    Add AuthContextProvider wrapper to provide auth state globally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext' // import your context
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
+    <AuthContextProvider>
       <App />
-    </AuthProvider>
+    </AuthContextProvider>
   </BrowserRouter>
-)
+);
