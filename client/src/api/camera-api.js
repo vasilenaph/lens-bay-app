54efe784ff5447c4ce9f0017fca9d7f64cf826e2
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2025-08-17 14:48:56 +0300

    Fix camera API export and add createCamera

--- client/src/api/camera-api.js
+++ client/src/api/camera-api.js
@@ -2,10 +2,12 @@ import * as request from './requester';
 
 const BASE_URL = 'http://localhost:5000/cameras';
 
-export const getAll = ()=> request.get(BASE_URL);
+export const getAll = () => request.get(BASE_URL);
+export const createCamera = (data) => request.post(`${BASE_URL}/create`, data);
 
 const camerasApi = {
-    getAll
-}
+    getAll,
+    createCamera
+};
 
 export default camerasApi;
