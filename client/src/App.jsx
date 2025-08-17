4a34c77a44441d8c2f3210ec2d6a27f13251f9d5
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2025-08-17 09:11:02 +0300

    Wrap App with AuthContextProvider to provide auth state globally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,31 +10,36 @@ import CreateCamera from './components/camera-create/cameraCreate';
 import SignIn from './components/sign-in/SignIn';
 import SignUp from './components/sign-up/SignUp';
 import CameraDetails from './components/camera-details/CameraDetails';
+import PrivateGuard from './components/common/PrivateGuard';
 
 export default function App() {
   return (
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            {/* Main Routes */}
-            <Route path="/" element={<Home />} />
-            <Route path="/cameras" element={<CameraList />} />
-            <Route path="/cameras/:cameraId" element={<CameraDetails />} />
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/cameras" element={<CameraList />} />
+          <Route path="/cameras/:cameraId" element={<CameraDetails />} />
+
+          {/* Auth Routes */}
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+
+          {/* Protected Routes */}
+          <Route element={<PrivateGuard />}>
             <Route path="/cameras/create" element={<CreateCamera />} />
-            
-            {/* Auth Routes */}
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            
-            {/* Other Routes */}
-            <Route path="/about" element={<About />} />
-            
-            {/* 404 Catch-all */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+          </Route>
+
+          {/* Other Routes */}
+          <Route path="/about" element={<About />} />
+
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
